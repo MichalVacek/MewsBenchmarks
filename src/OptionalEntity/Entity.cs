a733247117c1885d97f739117c85fbc947404899
michal.vacek <michal.vacek@mews.com>
2023-11-12 22:38:00 +0100

    Added Optional entity access benchmark

---
+++ src/OptionalEntity/Entity.cs
@@ -0,0 +1,54 @@
+﻿using FuncSharp;
+
+namespace MewsBenchmarks.OptionalEntity;
+
+public class Entity
+{
+    public Guid Id { get; set; }
+
+    public Guid? OptionalEntityId { get; set; }
+
+    public Entity OptionalEntityValue { get; set; }
+
+    public Entity? SafeOptionalEntity
+    {
+        get => ToOptional(OptionalEntityValue, OptionalEntityId);
+        internal set
+        {
+            OptionalEntityValue = value;
+            OptionalEntityId = OptionalEntityValue.MapRefToVal(v => v.Id);
+        }
+    }
+
+    public Option<Entity> OptionalEntity
+    {
+        get => ToOption(OptionalEntityValue, OptionalEntityId);
+        internal set
+        {
+            OptionalEntityValue = value.GetOrNull();
+            OptionalEntityId = OptionalEntityValue.MapRefToVal(v => v.Id);
+        }
+    }
+
+    protected Option<T> ToOption<T>(T entity, Guid? entityId)
+        where T : class
+    {
+        var isEntityLoaded = entity is not null || entityId is null;
+        if (!isEntityLoaded)
+        {
+            throw new Exception($"Property {typeof(T).Name} on entity {GetType().Name} is not loaded.");
+        }
+        return entity.ToOption();
+    }
+
+    protected T? ToOptional<T>(T entity, Guid? entityId)
+        where T : class
+    {
+        var isEntityLoaded = entity is not null || entityId is null;
+        if (!isEntityLoaded)
+        {
+            throw new Exception($"Property {typeof(T).Name} on entity {GetType().Name} is not loaded.");
+        }
+        return entity;
+    }
+}
---
+++ src/OptionalEntity/OptionalEnttityAccessBenchmark.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using FuncSharp;
+
+namespace MewsBenchmarks.OptionalEntity;
+
+[MemoryDiagnoser]
+public class OptionalEnttityAccessBenchmark
+{
+    public Entity PrimaryEntity = new Entity()
+    {
+        Id = Guid.NewGuid(),
+        OptionalEntityId = Guid.NewGuid(),
+        OptionalEntityValue = new Entity()
+        {
+            Id = Guid.NewGuid(),
+            OptionalEntityId = Guid.NewGuid(),
+            OptionalEntityValue = new Entity
+            {
+                Id = Guid.NewGuid()
+            }
+        }
+    };
+
+    [Benchmark]
+    public Guid? FuncSharpEntityAccessV1()
+    {
+        return PrimaryEntity.OptionalEntity.FlatMap(e => e.OptionalEntity.Map(e => e.Id)).ToNullable();
+    }
+
+    [Benchmark]
+    public Guid? FuncSharpEntityAccessV2()
+    {
+        return PrimaryEntity.OptionalEntity.Map(e => e.OptionalEntity.Map(e => e.Id)).Flatten().ToNullable();
+    }
+
+    [Benchmark]
+    public Guid? FuncSharpEntityAccessV3()
+    {
+
+        return PrimaryEntity.OptionalEntity.GetOrNull()?.OptionalEntity.GetOrNull()?.Id;
+    }
+
+    [Benchmark]
+    public Guid? NullConditionalOperatorsEntityAccess()
+    {
+        return PrimaryEntity.SafeOptionalEntity?.SafeOptionalEntity?.Id;
+    }
+}
+
